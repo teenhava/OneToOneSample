825d43ba15349be4ecd05c45b8282f80eb63ad24
developer <developer@therightdoctors.com>
2017-02-15 16:15:00 +0530

    Initial commit.jdfha.kwjdsas    tokbox sample

---
+++ app/src/main/java/com/tokbox/android/onetoonesample/config/OpenTokConfig.java
@@ -0,0 +1,14 @@
+package com.tokbox.android.onetoonesample.config;
+
+public class OpenTokConfig {
+
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+    // Replace with a generated Session ID
+    public static final String SESSION_ID = "2_MX40NTc1NzQ2Mn5-MTQ4NzE1NDAxNDgwNH4yWkRRbHZWS3BEUnZmSzYrWUhMRG4vdTB-fg";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTc1NzQ2MiZzaWc9NDM1MjFiYjI0YjM2YTVmZTFhOGEzYzI0Y2E2OWMwOTBkNTQxNTI5NTpzZXNzaW9uX2lkPTJfTVg0ME5UYzFOelEyTW41LU1UUTROekUxTkRBeE5EZ3dOSDR5V2tSUmJIWldTM0JFVW5abVN6WXJXVWhNUkc0dmRUQi1mZyZjcmVhdGVfdGltZT0xNDg3MTU0MDc0Jm5vbmNlPTAuODMwNDg5NjAxNTcwODE3NiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg3MjQwNDY4JmNvbm5lY3Rpb25fZGF0YT10b2tlbiUyMGZvciUyMHRva2JveA==";
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "45757462";
+
+}
